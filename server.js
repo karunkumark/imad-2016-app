8280dfd2696927a222e02d9cc8f26918edea1fd7
karunkumark <karunk911@gmail.com>
2016-10-16 13:34:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var articles = {
 
 function createTemplate(data){
     var title = data.title;
-    var date = data.title;
+    var date = data.date;
     var heading = data.heading;
     var content = data.content;
     
