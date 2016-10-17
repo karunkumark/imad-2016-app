222dc8021cf9eb04f724edf69562a8a89452a6e6
karunkumark <karunk911@gmail.com>
2016-10-17 21:51:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ element.innerHTML ='New Value';
 var img = document.getElementById('maid');
 
 img.onclick = function () {
-    img.style.marginLeft = "100px";
+    img.style.marginLeft = "200px";
 }
