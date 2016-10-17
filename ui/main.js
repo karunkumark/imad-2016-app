c32734dcf98d57dae374f2d1611a6fd439f2d4d6
karunkumark <karunk911@gmail.com>
2016-10-17 22:10:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,14 +4,14 @@ var element = document.getElementById('main-text');
 
 element.innerHTML ='New Value';
 
-var img = document.getElementById('madi');
+var maintext = document.getElementById('main-tetx');
 var mariginLeft = 0;
 
 function moveRight() {
     marginLeft =marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+    maintext.style.marginLeft = marginLeft + 'px';
 }
 
-img.onclick = function () {
+maintext.onclick = function () {
     var interval = setInterval(moveRight,50);
 }
