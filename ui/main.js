f9f9fab8dd37f70999c3e6394fb59849bafdbf5b
karunkumark <karunk911@gmail.com>
2016-10-17 22:12:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,14 +4,14 @@ var element = document.getElementById('main-text');
 
 element.innerHTML ='New Value';
 
-var maintext = document.getElementById('main-text');
+var maintext = document.getElementById('madi');
 var mariginLeft = 0;
 
 function moveRight() {
     marginLeft =marginLeft + 1;
-    maintext.style.marginLeft = marginLeft + 'px';
+    madi.style.marginLeft = marginLeft + 'px';
 }
 
-maintext.onclick = function () {
+madi.onclick = function () {
     var interval = setInterval(moveRight,50);
 }
